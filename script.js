f1705eecd6323321eaf7889d58fde17e2ee98f95
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-09-20 18:24:24 -0300

    add page turn animation on new game

--- script.js
+++ script.js
@@ -4,11 +4,18 @@ const
   line3 = document.querySelector(".line.l3"),
   line4 = document.querySelector(".line.l4"),
   hand = document.querySelector(".hand"),
-  btn = document.querySelector("button");
+  btnStartGame = document.querySelector(".btn-start-game"),
+  pageTurn = document.querySelector(".page-turn");
 
-btn.addEventListener("click", () => {
-  [line1,line2,line3,line4,hand].forEach(line => {
-    line.classList.add("animate")
-  });
-  btn.classList.add("hidden");
+  btnStartGame.addEventListener("click", () => {
+  pageTurn.classList.add("animate");
+  btnStartGame.classList.add("hidden");
+});
+
+pageTurn.addEventListener("transitionend", (e) => {
+  if(e.propertyName === "left"){
+    [line1,line2,line3,line4,hand,].forEach(line => {
+      line.classList.add("animate")
+    });
+  };
 });

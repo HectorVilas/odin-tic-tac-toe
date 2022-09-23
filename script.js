dea68ba4baae7d12354b87d36cd278f6751a1d92
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-09-22 23:21:14 -0300

    move listeners and new game action to function

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-const
-  line1 = document.querySelector(".line.l1"),
-  line2 = document.querySelector(".line.l2"),
-  line3 = document.querySelector(".line.l3"),
-  line4 = document.querySelector(".line.l4"),
-  hand = document.querySelector(".hand"),
-  btnStartGame = document.querySelector(".btn-start-game"),
-  pageTurn = document.querySelector(".page-turn");
-
-btnStartGame.addEventListener("click", () => {
-  pageTurn.classList.remove("animate")
-  setTimeout(() => {
-    pageTurn.classList.add("animate")
-    
-  }, 100);
-
-  setTimeout(() => {
-    [line1,line2,line3,line4,hand].forEach(line => {
-      line.classList.remove("animate")
-    });
+const displayController = (() => {
+  const
+    line1 = document.querySelector(".line.l1"),
+    line2 = document.querySelector(".line.l2"),
+    line3 = document.querySelector(".line.l3"),
+    line4 = document.querySelector(".line.l4"),
+    hand = document.querySelector(".hand"),
+    btnStartGame = document.querySelector(".btn-start-game"),
+    pageTurn = document.querySelector(".page-turn");
+  
+  function addListeners(){
+    btnStartGame.addEventListener("click", newMatch);
+  };
+  
+  
+  function newMatch(){
+    pageTurn.classList.remove("animate")
+    setTimeout(() => {
+      pageTurn.classList.add("animate")
+      
+    }, 100);
+  
     setTimeout(() => {
       [line1,line2,line3,line4,hand].forEach(line => {
-        line.classList.add("animate")
+        line.classList.remove("animate")
       });
-    }, 100);
-  }, 300);
-  // btnStartGame.classList.add("hidden");
-});
+      setTimeout(() => {
+        [line1,line2,line3,line4,hand].forEach(line => {
+          line.classList.add("animate")
+        });
+      }, 100);
+    }, 300);
+    // btnStartGame.classList.add("hidden");
+  };
+  
+  return {addListeners, newMatch}
+})();
+
+//run on start
+displayController.addListeners();

c0e9638a775ba3b81365a86677163d3bad048eb9
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-09-24 19:11:56 -0300

    add win condition

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const gameBoard = (() => {
-  let playerOneTurn = true;
-  let board = new Array(9).fill(0);
+const displayController = (() => {
+  
   const lines = document.querySelectorAll(".line");
   const cells = document.querySelectorAll(".cell");
   //animation related
@@ -14,13 +13,11 @@ const gameBoard = (() => {
     
     cells.forEach((cell, i) => {
       cell.addEventListener("click", () => {
-        if(board[i] === 0){
-          let num;
-          playerOneTurn ? num = 1 : num = 2;
-          board[i] = num;
-          cell.innerText = num;
-          playerOneTurn = !playerOneTurn;
-          console.log(board);
+        if(gameFlow.board[i] === 0){
+          gameFlow.board[i] = gameFlow.placeSymbol();
+          cell.innerText = gameFlow.placeSymbol();
+          gameFlow.winConditions();
+          gameFlow.swapPlayer();
         };
       });
     });
@@ -36,21 +33,58 @@ const gameBoard = (() => {
       });
       //reset game board
       cells.forEach(c => c.innerText = "");
-      board.fill(0);
-      playerOneTurn = true;
+      gameFlow.board.fill(0);
+      // gameFlow.playerOneTurn = true;
 
       setTimeout(() => {
         [...lines,hand].forEach(line => {
-          line.classList.add("animate")
+          line.classList.add("animate");
         });
       }, 100);
     }, 300);
   };
 
-  return { addListeners }
+  return { addListeners };
 })();
 
+const gameFlow = (() => {
+  let playerOneTurn = true;
+  let board = new Array(9).fill(0);
+
+  function swapPlayer(){
+    playerOneTurn = !playerOneTurn;
+  }
+  function placeSymbol(){
+    if(playerOneTurn) {
+      return 1;
+    } else {
+      return 2;
+    };
+    
+  }
+  function winConditions(){
+    if(
+      //horizontal
+      board[0] === board[1] && board[1] === board[2] && board[0] !== 0 ||
+      board[3] === board[4] && board[4] === board[5] && board[3] !== 0 ||
+      board[6] === board[7] && board[7] === board[8] && board[6] !== 0 ||
+      //vertical
+      board[0] === board[3] && board[3] === board[6] && board[0] !== 0 ||
+      board[1] === board[4] && board[4] === board[7] && board[1] !== 0 ||
+      board[2] === board[5] && board[5] === board[8] && board[2] !== 0 ||
+      //diagonal
+      board[0] === board[4] && board[4] === board[8] && board[0] !== 0 ||
+      board[2] === board[4] && board[4] === board[6] && board[2] !== 0 
+    ){
+      let winner;
+      playerOneTurn ? winner = "player 1" : winner = "player 2";
+      console.log(`${winner} wins! Flawless victory.`);
+    }
+  };
+  
+  return {winConditions, board, swapPlayer, placeSymbol};
+})()
 
 
 //run on start
-gameBoard.addListeners();
+displayController.addListeners();
